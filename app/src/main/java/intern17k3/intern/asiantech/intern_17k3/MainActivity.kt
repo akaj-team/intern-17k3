d2bddd50acc695d34f98c7fe7c7ac7f5d832ca91
vinhhuynhl.b <vinh.huynh@asiantech.vn>
2017-11-22 13:59:15 +0700

    first init

---
+++ app/src/main/java/intern17k3/intern/asiantech/intern_17k3/MainActivity.kt
@@ -0,0 +1,12 @@
+package intern17k3.intern.asiantech.intern_17k3
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/intern17k3/intern/asiantech/intern_17k3/anhquach/IndexActivity.java
@@ -0,0 +1,10 @@
+package intern17k3.intern.asiantech.intern_17k3.anhquach;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Copyright © 2017 AsianTech inc.
+ * Created by vinh.huynh on 11/22/17.
+ */
+public class IndexActivity extends AppCompatActivity {
+}
