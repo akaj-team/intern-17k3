02cc6832a93989a9f5a9e89cc2f97bf837de8afe
Tien Hoang Ngoc <tienhn@asiantech.vn>
2017-11-23 10:14:32 +0700

    Update base project

---
+++ app/src/at-nhatnguyen/java/vn/asiantech/internship/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.asiantech.internship;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
