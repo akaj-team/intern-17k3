bbe539a0e376689cd82778fd448cab3888f14daf
Tien Hoang Ngoc <tienhn@asiantech.vn>
2017-11-23 12:04:53 +0700

    Update scropt

--- scripts/saddler.sh
+++ scripts/saddler.sh
@@ -41,7 +41,7 @@ cat app/build/reports/pmd/cpd.xml \
 echo "********************"
 echo "* android lint     *"
 echo "********************"
-cat app/build/outputs/lint-results.xml \
+cat app/build/reports/lint-results.xml \
     | bundle exec android_lint_translate_checkstyle_format translate \
     | bundle exec checkstyle_filter-git diff origin/master \
     | bundle exec saddler report --require saddler/reporter/github --reporter $REPORTER
