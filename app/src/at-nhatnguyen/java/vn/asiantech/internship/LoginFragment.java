22d5d9b1a99b2d0b4a3602d9e3bed44c50c45973
at-nhatnguyen <nhat.nguyen@asiantech.vn>
2017-11-24 09:49:59 +0700

    change something

---
+++ app/src/at-nhatnguyen/java/vn/asiantech/internship/LoginFragment.java
@@ -0,0 +1,32 @@
+package vn.asiantech.internship;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container,savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        Button btnLoginFacebook;
+        btnLoginFacebook = (Button) view.findViewById(R.id.btnLoginFacebook);
+        btnLoginFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ( ( MainActivity) getActivity()).callFragment(new InforFragment());
+            }
+        });
+        return view;
+    }
+}
--- app/src/at-nhatnguyen/java/vn/asiantech/internship/MainActivity.java
+++ app/src/at-nhatnguyen/java/vn/asiantech/internship/MainActivity.java
@@ -1,14 +1,28 @@
 package vn.asiantech.internship;
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        callFragment(new LoginFragment());
+    }
+
+    public void callFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        //Khi được goi, fragment truyền vào sẽ thay thế vào vị trí FrameLayout trong Activity chính
+        transaction.replace(R.id.fmContent,fragment);
+        transaction.commit();
     }
 
 }
