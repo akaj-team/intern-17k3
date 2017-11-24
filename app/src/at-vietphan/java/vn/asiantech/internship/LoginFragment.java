5cf0367979ac5cc362cca872d7f5b63ab6a4594c
at-vietphan <viet.phan@asiantech.vn>
2017-11-24 10:57:00 +0700

    create_login_screen

--- app/src/at-vietphan/java/vn/asiantech/internship/LoginFragment.java
+++ app/src/at-vietphan/java/vn/asiantech/internship/LoginFragment.java
@@ -3,7 +3,6 @@ package vn.asiantech.internship;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
