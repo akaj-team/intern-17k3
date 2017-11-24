d9d47fa046c5b892efc868ab1761551a1794d360
tungnguyen2 <tung.nguyen2@asiantech.vngit config --global user.email tung.nguyen2@asiantech.vngit config --global user.email tung.nguyen2@asiantech.v>
2017-11-24 08:58:26 +0700

    update

---
+++ app/src/at-tungnguyen/java/vn/asiantech/internship/LoginFragment.java
@@ -0,0 +1,34 @@
+package vn.asiantech.internship;
+
+import android.support.v4.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by tungnguyen on 23/11/2017.
+ */
+
+public class LoginFragment extends Fragment {
+    private TextView mtvSignup;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_login,container,false);
+        mtvSignup = view.findViewById(R.id.tvSignup);
+        mtvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).Replace(new SignUpFragment());
+            }
+        });
+        return view;
+    }
+}
--- app/src/at-tungnguyen/java/vn/asiantech/internship/MainActivity.java
+++ app/src/at-tungnguyen/java/vn/asiantech/internship/MainActivity.java
@@ -4,12 +4,23 @@ package vn.asiantech.internship;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Replace(new LoginFragment());
     }
-
+    public  void Replace(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frgContainer,fragment).commit();
+    }
 }
