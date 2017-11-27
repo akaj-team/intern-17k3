8039b31e256d6ca652efee3a61d8d856afc4d328
Viet Phan X <viet.phan@asiantech.vn>
2017-11-27 15:54:49 +0700

    login_LTS

--- app/src/at-vietphan/java/vn/asiantech/internship/IndexFragment.java
+++ app/src/at-vietphan/java/vn/asiantech/internship/IndexFragment.java
@@ -1,12 +1,20 @@
 package vn.asiantech.internship;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class IndexFragment extends Fragment {
     public static IndexFragment newInstance() {
@@ -29,6 +37,27 @@ public class IndexFragment extends Fragment {
                 ((MainActivity) getActivity()).getmImgBack().setVisibility(View.VISIBLE);
             }
         });
+        TextView tvMemoLogin = view.findViewById(R.id.tvMemoLogin);
+        SpannableString spannableString = new SpannableString(tvMemoLogin.getText());
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).replaceFragment(new LoginFragment(), true);
+                ((MainActivity) getActivity()).getmImgBack().setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.bgColor = Color.WHITE;
+                ds.setARGB(255, 255, 255, 255);
+                ds.setUnderlineText(false);
+                ds.setColor(ContextCompat.getColor(getContext(), R.color.colorSignupAcc));
+            }
+        };
+        spannableString.setSpan(clickableSpan, 12, tvMemoLogin.getText().length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tvMemoLogin.setText(spannableString);
+        tvMemoLogin.setMovementMethod(LinkMovementMethod.getInstance());
         return view;
     }
 }
