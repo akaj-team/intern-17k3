1dea4ebbd2d6baaf86d0311396cd8b9fd45a605d
Anh Quach T. N <anh.quach@asiantech.vn>
2017-11-27 18:24:48 +0700

    fixed import

--- app/src/at-anhquach/java/vn/asiantech/internship/ui/login/InfoEnterFragment.java
+++ app/src/at-anhquach/java/vn/asiantech/internship/ui/login/InfoEnterFragment.java
@@ -12,7 +12,6 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
-import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
