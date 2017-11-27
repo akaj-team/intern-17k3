7f923bea227e8aa04248f109534bcdf41caea48a
Phong Le T <phong.le@asiantech.vn>
2017-11-27 09:14:47 +0700

    update back button

--- app/src/at-phongle/java/vn/asiantech/internship/login/LoginActivity.java
+++ app/src/at-phongle/java/vn/asiantech/internship/login/LoginActivity.java
@@ -5,11 +5,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 import vn.asiantech.internship.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView mImgBack;
 
     @Override
@@ -17,15 +19,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mImgBack = findViewById(R.id.imgBack);
+        mImgBack.setOnClickListener(this);
         addFragment(LoginFragment.getInstance(), false);
     }
 
     protected void addFragment(Fragment fragment, boolean addBackStack) {
-        if (addBackStack) {
-            mImgBack.setSelected(true);
-        } else {
-            mImgBack.setSelected(false);
-        }
+        mImgBack.setSelected(addBackStack);
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frContainer, fragment);
@@ -33,5 +32,18 @@ public class LoginActivity extends AppCompatActivity {
             fragmentTransaction.addToBackStack(fragment.getTag());
         }
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        addFragment(new LoginFragment(), false);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            mImgBack.setSelected(false);
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
