cd6d6b2eb8be8e25c553b0bdd9cedc59088a83f4
Anh Quach T. N <anh.quach@asiantech.vn>
2017-11-28 15:16:14 +0700

    make calculator view

--- app/src/at-anhquach/java/vn/asiantech/internship/CalculatorViewActivity.java
+++ app/src/at-anhquach/java/vn/asiantech/internship/CalculatorViewActivity.java
@@ -1,8 +1,8 @@
 package vn.asiantech.internship;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import vn.asiantech.internship.R;
-
-public class CalculatorViewActivity extends Activity implements View.OnClickListener {
-    private EditText mEdtInputA;
+public class CalculatorViewActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText mEdtNumberA;
     private EditText mEdtInputB;
     private TextView mTvOperator;
     private TextView mTvResutl;
@@ -21,12 +19,68 @@ public class CalculatorViewActivity extends Activity implements View.OnClickList
     private Button mBtnSub;
     private Button mBtnMulti;
     private Button mBtnDiv;
+    private float result = 0;
+    private String oper = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator_view);
-        mEdtInputA = findViewById(R.id.edtInputA);
+        init();
+        onClickListener();
+    }
+
+    @Override
+    public void onClick(View view) {
+        float numA;
+        float numB;
+
+        if (TextUtils.isEmpty(mEdtNumberA.getText().toString())
+                || TextUtils.isEmpty(mEdtInputB.getText().toString())) {
+            return;
+        }
+
+        numA = Float.parseFloat(mEdtNumberA.getText().toString());
+        numB = Float.parseFloat(mEdtInputB.getText().toString());
+
+        switch (view.getId()) {
+            case R.id.btnAdd:
+                oper = this.getString(R.string.Add);
+                result = numA + numB;
+                convertFloattoInt();
+                break;
+            case R.id.btnSub:
+                oper = this.getString(R.string.Sub);
+                result = numA - numB;
+                convertFloattoInt();
+                break;
+            case R.id.btnMulti:
+                oper = this.getString(R.string.Multi);
+                result = numA * numB;
+                convertFloattoInt();
+                break;
+            case R.id.btnDiv:
+                oper = this.getString(R.string.Divide);
+                if (numB != 0) {
+                    result = numA / numB;
+                    convertFloattoInt();
+                } else {
+                    Context context = getApplicationContext();
+                    CharSequence text = context.getString(R.string.NoteDivideZero);
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                    mTvResutl.setText("");
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void init() {
+        mEdtNumberA = findViewById(R.id.edtInputA);
         mEdtInputB = findViewById(R.id.edtInputB);
         mTvOperator = findViewById(R.id.tvOperator);
         mTvResutl = findViewById(R.id.tvResutl);
@@ -34,57 +88,22 @@ public class CalculatorViewActivity extends Activity implements View.OnClickList
         mBtnSub = findViewById(R.id.btnSub);
         mBtnMulti = findViewById(R.id.btnMulti);
         mBtnDiv = findViewById(R.id.btnDiv);
+    }
 
+    private void onClickListener() {
         mBtnAdd.setOnClickListener(this);
         mBtnSub.setOnClickListener(this);
         mBtnMulti.setOnClickListener(this);
         mBtnDiv.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        float numA=0;
-        float numB=0;
-        float result=0;
-        String oper="";
-
-        if (TextUtils.isEmpty(mEdtInputA.getText().toString())
-                || TextUtils.isEmpty(mEdtInputB.getText().toString())) {
-            return;
+    private void convertFloattoInt() {
+        int intResult = (int) result;
+        if (result == intResult) {
+            mTvResutl.setText(String.valueOf(intResult));
+        } else {
+            mTvResutl.setText(String.valueOf(result));
         }
-
-        numA = Float.parseFloat(mEdtInputA.getText().toString());
-        numB = Float.parseFloat(mEdtInputB.getText().toString());
-
-        switch (view.getId()) {
-            case R.id.btnAdd:
-                oper = "+";
-                result = numA + numB;
-                break;
-            case R.id.btnSub:
-                oper = "-";
-                result = numA - numB;
-                break;
-            case R.id.btnMulti:
-                oper = "*";
-                result = numA * numB;
-                break;
-            case R.id.btnDiv:
-                oper = "/";
-                if (numB!=0){
-                    result = numA / numB;
-                }else{
-                    Context context = getApplicationContext();
-                    CharSequence text = "Cant divide for 0";
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                }
-                break;
-            default:
-                break;
-        }
-        mTvResutl.setText(""+result);
         mTvOperator.setText(oper);
     }
 }
