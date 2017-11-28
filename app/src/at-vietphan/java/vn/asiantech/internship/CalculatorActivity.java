f766c872ff5cd978fc4bef67f7cd456e7ad430d6
Viet Phan X <viet.phan@asiantech.vn>
2017-11-28 14:16:55 +0700

    change style code

--- app/src/at-vietphan/java/vn/asiantech/internship/CalculatorActivity.java
+++ app/src/at-vietphan/java/vn/asiantech/internship/CalculatorActivity.java
@@ -25,13 +25,13 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-        initView();
-        checkAllInputData();
-        implementClick();
+        initViews();
+        setTextWatcher();
+        implementClicks();
 
     }
 
-    private void initView() {
+    private void initViews() {
         mBtnPlus = findViewById(R.id.btnPlus);
         mBtnMinus = findViewById(R.id.btnMinus);
         mBtnMultiplication = findViewById(R.id.btnMultiplication);
@@ -42,75 +42,49 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
         mTvResult = findViewById(R.id.tvResult);
     }
 
-    private void implementClick() {
+    private void implementClicks() {
         mBtnPlus.setOnClickListener(this);
         mBtnMinus.setOnClickListener(this);
         mBtnMultiplication.setOnClickListener(this);
         mBtnDivision.setOnClickListener(this);
     }
 
-    private void checkAllInputData() {
-        checkEdtInputData(mEdtNumberA);
-        checkEdtInputData(mEdtNumberB);
+    private void setTextWatcher() {
+        mEdtNumberA.addTextChangedListener(textWatcher);
+        mEdtNumberB.addTextChangedListener(textWatcher);
     }
 
-    private void checkEdtInputData(EditText editText) {
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                // No-op
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                // No-op
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (TextUtils.isEmpty(mEdtNumberA.getText()) || TextUtils.isEmpty(mEdtNumberB.getText())) {
-                    mBtnPlus.setEnabled(false);
-                    mBtnMinus.setEnabled(false);
-                    mBtnMultiplication.setEnabled(false);
-                    mBtnDivision.setEnabled(false);
-                    Toast.makeText(CalculatorActivity.this, "Number A or Number B is Empty !", Toast.LENGTH_LONG).show();
-                } else {
-                    mBtnPlus.setEnabled(true);
-                    mBtnMinus.setEnabled(true);
-                    mBtnMultiplication.setEnabled(true);
-                    mBtnDivision.setEnabled(true);
-                }
-            }
-        });
+    private void isEnableBtn(Boolean aBoolean) {
+        mBtnPlus.setEnabled(aBoolean);
+        mBtnMinus.setEnabled(aBoolean);
+        mBtnMultiplication.setEnabled(aBoolean);
+        mBtnDivision.setEnabled(aBoolean);
     }
 
     @Override
     public void onClick(View view) {
-        double result;
+        double numberA = Double.parseDouble(mEdtNumberA.getText().toString().trim());
+        double numberB = Double.parseDouble(mEdtNumberB.getText().toString().trim());
         switch (view.getId()) {
             case R.id.btnPlus:
                 mTvCalculation.setText("+");
-                result = plus(Double.parseDouble(mEdtNumberA.getText().toString()), Double.parseDouble(mEdtNumberB.getText().toString()));
-                mTvResult.setText(String.valueOf(result));
+                mTvResult.setText(String.valueOf(plus(numberA,numberB)));
                 break;
             case R.id.btnMinus:
                 mTvCalculation.setText("-");
-                result = minus(Double.parseDouble(mEdtNumberA.getText().toString()), Double.parseDouble(mEdtNumberB.getText().toString()));
-                mTvResult.setText(String.valueOf(result));
+                mTvResult.setText(String.valueOf(minus(numberA,numberB)));
                 break;
             case R.id.btnMultiplication:
                 mTvCalculation.setText("*");
-                result = multiplication(Double.parseDouble(mEdtNumberA.getText().toString()), Double.parseDouble(mEdtNumberB.getText().toString()));
-                mTvResult.setText(String.valueOf(result));
+                mTvResult.setText(String.valueOf(multiplication(numberA,numberB)));
                 break;
             case R.id.btnDivision:
-                if (Integer.parseInt(mEdtNumberB.getText().toString()) == 0) {
+                if (Double.parseDouble(mEdtNumberB.getText().toString()) == 0) {
                     mTvResult.setText("");
                     Toast.makeText(CalculatorActivity.this, "Number B other number 0", Toast.LENGTH_SHORT).show();
                 } else {
                     mTvCalculation.setText("/");
-                    result = division(Double.parseDouble(mEdtNumberA.getText().toString()), Double.parseDouble(mEdtNumberB.getText().toString()));
-                    mTvResult.setText(String.valueOf(result));
+                    mTvResult.setText(String.valueOf(division(numberA,numberB)));
                 }
                 break;
         }
@@ -131,5 +105,27 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
     private double division(double a, double b) {
         return a / b;
     }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            // No-op
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            // No-op
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if (TextUtils.isEmpty(mEdtNumberA.getText()) || TextUtils.isEmpty(mEdtNumberB.getText())) {
+                isEnableBtn(false);
+                Toast.makeText(CalculatorActivity.this, "Number A or Number B is Empty !", Toast.LENGTH_LONG).show();
+            } else {
+                isEnableBtn(true);
+            }
+        }
+    };
 }
 
