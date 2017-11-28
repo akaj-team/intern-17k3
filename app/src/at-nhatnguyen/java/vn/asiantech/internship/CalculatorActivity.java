a239dbcbce286901d7564cb63b7e28fb6005a369
at-nhatnguyen <nhat.nguyen@asiantech.vn>
2017-11-28 14:36:49 +0700

    complete calculator screen

--- app/src/at-nhatnguyen/java/vn/asiantech/internship/CalculatorActivity.java
+++ app/src/at-nhatnguyen/java/vn/asiantech/internship/CalculatorActivity.java
@@ -2,86 +2,107 @@ package vn.asiantech.internship;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener, TextWatcher {
+    private static final String NOTIFY_NUMBER_TWO = "Second number can't 0";
     private EditText mEdtNumOne;
     private EditText mEdtNumTwo;
     private TextView mTvResult;
     private TextView mTvCalculator;
+    private Button mBtnSummation;
+    private Button mBtnSubtraction;
+    private Button mBtnMultiplication;
+    private Button mBtnDivision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        initItem();
+        initListener();
+    }
+
+    private void initItem() {
         mEdtNumOne = findViewById(R.id.edtNumOne);
         mEdtNumTwo = findViewById(R.id.edtNumTwo);
         mTvResult = findViewById(R.id.tvResult);
         mTvCalculator = findViewById(R.id.tvCalculator);
-        Button mBtnSummation = findViewById(R.id.btnSummation);
-        Button mBtnSubtraction = findViewById(R.id.btnSubtraction);
-        Button mBtnMultiplication = findViewById(R.id.btnMultiplication);
-        Button mBtnDivision = findViewById(R.id.btnDivision);
-        mBtnSummation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mEdtNumOne.getText().length() == 0 || mEdtNumTwo.getText().length() == 0) {
-                    Toast.makeText(CalculatorActivity.this, "Input all", Toast.LENGTH_SHORT).show();
-                } else {
-                    mTvCalculator.setText("+");
-                    float a = summation(Float.parseFloat(mEdtNumOne.getText().toString()), Float.parseFloat(mEdtNumTwo.getText().toString()));
-                    mTvResult.setText("" + a);
-                }
-            }
-        });
+        mBtnSummation = findViewById(R.id.btnSummation);
+        mBtnSubtraction = findViewById(R.id.btnSubtraction);
+        mBtnMultiplication = findViewById(R.id.btnMultiplication);
+        mBtnDivision = findViewById(R.id.btnDivision);
+    }
 
-        mBtnSubtraction.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mEdtNumOne.getText().length() == 0 || mEdtNumTwo.getText().length() == 0) {
-                    Toast.makeText(CalculatorActivity.this, "Input all", Toast.LENGTH_SHORT).show();
-                } else {
-                    mTvCalculator.setText("-");
-                    float a = subtraction(Float.parseFloat(mEdtNumOne.getText().toString()), Float.parseFloat(mEdtNumTwo.getText().toString()));
-                    mTvResult.setText("" + a);
-                }
-            }
-        });
+    private void initListener() {
+        mEdtNumOne.addTextChangedListener(this);
+        mEdtNumTwo.addTextChangedListener(this);
+        mBtnSummation.setOnClickListener(this);
+        mBtnSubtraction.setOnClickListener(this);
+        mBtnMultiplication.setOnClickListener(this);
+        mBtnDivision.setOnClickListener(this);
+    }
 
-        mBtnMultiplication.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mEdtNumOne.getText().length() == 0 || mEdtNumTwo.getText().length() == 0) {
-                    Toast.makeText(CalculatorActivity.this, "Input all", Toast.LENGTH_SHORT).show();
-                } else {
-                    mTvCalculator.setText("*");
-                    float a = multiplication(Float.parseFloat(mEdtNumOne.getText().toString()), Float.parseFloat(mEdtNumTwo.getText().toString()));
-                    mTvResult.setText("" + a);
-                }
-            }
-        });
+    private void setEnabledButton(Boolean mBoolean) {
+        mBtnSummation.setEnabled(mBoolean);
+        mBtnSubtraction.setEnabled(mBoolean);
+        mBtnMultiplication.setEnabled(mBoolean);
+        mBtnDivision.setEnabled(mBoolean);
+    }
 
-        mBtnDivision.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mEdtNumOne.getText().length() == 0 || mEdtNumTwo.getText().length() == 0) {
-                    Toast.makeText(CalculatorActivity.this, "Input all", Toast.LENGTH_SHORT).show();
+    @Override
+    public void onClick(View view) {
+        float a;
+        switch (view.getId()) {
+            case R.id.btnSummation:
+                mTvCalculator.setText(R.string.summation);
+                a = summation(Float.parseFloat(mEdtNumOne.getText().toString()), Float.parseFloat(mEdtNumTwo.getText().toString()));
+                mTvResult.setText(String.valueOf(a));
+                break;
+            case R.id.btnSubtraction:
+                mTvCalculator.setText(R.string.subtraction);
+                a = subtraction(Float.parseFloat(mEdtNumOne.getText().toString()), Float.parseFloat(mEdtNumTwo.getText().toString()));
+                mTvResult.setText(String.valueOf(a));
+                break;
+            case R.id.btnMultiplication:
+                mTvCalculator.setText(R.string.multiplication);
+                a = multiplication(Float.parseFloat(mEdtNumOne.getText().toString()), Float.parseFloat(mEdtNumTwo.getText().toString()));
+                mTvResult.setText(String.valueOf(a));
+                break;
+            case R.id.btnDivision:
+                if (Integer.parseInt(mEdtNumTwo.getText().toString()) == 0) {
+                    showToastNotify(NOTIFY_NUMBER_TWO);
                 } else {
-                    if (Integer.parseInt(mEdtNumTwo.getText().toString()) == 0) {
-                        Toast.makeText(CalculatorActivity.this, "Num two isn't 0", Toast.LENGTH_SHORT).show();
-                    } else {
-                        mTvCalculator.setText("/");
-                        float a = division(Float.parseFloat(mEdtNumOne.getText().toString()), Float.parseFloat(mEdtNumTwo.getText().toString()));
-                        mTvResult.setText("" + a);
-                    }
+                    mTvCalculator.setText(R.string.division);
+                    a = division(Float.parseFloat(mEdtNumOne.getText().toString()), Float.parseFloat(mEdtNumTwo.getText().toString()));
+                    mTvResult.setText(String.valueOf(a));
                 }
-            }
-        });
+        }
+    }
 
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        //NO-OP
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        //NO-OP
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        if (mEdtNumOne.getText().length() == 0 || mEdtNumTwo.getText().length() == 0) {
+            setEnabledButton(false);
+        } else {
+            setEnabledButton(true);
+        }
     }
 
     private float summation(float a, float b) {
@@ -98,5 +119,9 @@ public class CalculatorActivity extends AppCompatActivity {
 
     private float division(float a, float b) {
         return a / b;
+    }
+
+    private void showToastNotify(String notify) {
+        Toast.makeText(CalculatorActivity.this, notify, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/at-nhatnguyen/java/vn/asiantech/internship/MainActivity.java
+++ app/src/at-nhatnguyen/java/vn/asiantech/internship/MainActivity.java
@@ -1,7 +1,10 @@
 package vn.asiantech.internship;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +12,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnxEercise2 = findViewById(R.id.btnExercise2);
+        btnxEercise2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
 }
