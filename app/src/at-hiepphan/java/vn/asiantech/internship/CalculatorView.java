c8195e895dd01239971479171a35ec23a57f5eb7
Hiep Phan <hiep.phan@asiantech.vn>
2017-11-28 23:40:34 +0700

    Implement View.OnClickListener, handle on Edittext change

--- app/src/at-hiepphan/java/vn/asiantech/internship/CalculatorView.java
+++ app/src/at-hiepphan/java/vn/asiantech/internship/CalculatorView.java
@@ -2,13 +2,17 @@ package vn.asiantech.internship;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
-public class CalculatorView extends AppCompatActivity {
+public class CalculatorView extends AppCompatActivity implements View.OnClickListener {
     private EditText mEdtFirstNumber;
     private EditText mEdtSecondNumber;
     private Button mBtnAddition;
@@ -22,6 +26,12 @@ public class CalculatorView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator_view);
+        initView();
+        onClickListener();
+        onEditTextChange();
+    }
+
+    private void initView() {
         mEdtFirstNumber = findViewById(R.id.edtFirstNumber);
         mEdtSecondNumber = findViewById(R.id.edtSecondNumber);
         mBtnAddition = findViewById(R.id.btnAddition);
@@ -30,77 +40,119 @@ public class CalculatorView extends AppCompatActivity {
         mBtnDivision = findViewById(R.id.btnDivision);
         mTvOperator = findViewById(R.id.tvOperator);
         mTvResult = findViewById(R.id.tvResult);
+    }
 
-        mBtnAddition.setOnClickListener(new View.OnClickListener() {
+    private void onClickListener() {
+        mBtnAddition.setOnClickListener(this);
+        mBtnSubtraction.setOnClickListener(this);
+        mBtnMultiplication.setOnClickListener(this);
+        mBtnDivision.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (checkInput()) {
+            Button btnOnClick = findViewById(view.getId());
+            String operator = btnOnClick.getText().toString();
+            calculate(operator);
+        } else {
+            mTvResult.setText("");
+        }
+    }
+
+    private void onEditTextChange() {
+        mEdtFirstNumber.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                //mTvResult.setText("");
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // No-op
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String operator = mTvOperator.getText().toString();
+                if (operator.length() != 0 && mEdtFirstNumber.length() != 0 && mEdtSecondNumber.length() != 0) {
+                    calculate(operator);
+                } else {
+                    mTvResult.setText("");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // No-op
+            }
+        });
+
+        mEdtSecondNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // No-op
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String operator = mTvOperator.getText().toString();
+                if (operator.length() != 0 && mEdtFirstNumber.length() != 0 && mEdtSecondNumber.length() != 0) {
+                    calculate(operator);
+                } else {
+                    mTvResult.setText("");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // No-op
+            }
+        });
+    }
+
+    private void calculate(String operator) {
+        BigDecimal firstNumber = new BigDecimal(mEdtFirstNumber.getText().toString());
+        BigDecimal secondNumber = new BigDecimal(mEdtSecondNumber.getText().toString());
+        switch (operator) {
+            case "+":
                 mTvOperator.setText("+");
-                if (checkInput()) {
-                    final float firstNumber = Float.parseFloat(mEdtFirstNumber.getText().toString());
-                    final float secondNumber = Float.parseFloat(mEdtSecondNumber.getText().toString());
-                    addition(firstNumber, secondNumber);
-                }
-            }
-        });
-
-        mBtnSubtraction.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                addition(firstNumber, secondNumber);
+                break;
+            case "-":
                 mTvOperator.setText("-");
-                if (checkInput()) {
-                    final float firstNumber = Float.parseFloat(mEdtFirstNumber.getText().toString());
-                    final float secondNumber = Float.parseFloat(mEdtSecondNumber.getText().toString());
-                    subtraction(firstNumber, secondNumber);
-                }
-            }
-        });
-
-        mBtnMultiplication.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                subtraction(firstNumber, secondNumber);
+                break;
+            case "*":
                 mTvOperator.setText("*");
-                if (checkInput()) {
-                    final float firstNumber = Float.parseFloat(mEdtFirstNumber.getText().toString());
-                    final float secondNumber = Float.parseFloat(mEdtSecondNumber.getText().toString());
-                    multiplication(firstNumber, secondNumber);
-                }
-            }
-        });
-
-        mBtnDivision.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                multiplication(firstNumber, secondNumber);
+                break;
+            case "/":
                 mTvOperator.setText("/");
-                if (checkInput()) {
-                    final float firstNumber = Float.parseFloat(mEdtFirstNumber.getText().toString());
-                    final float secondNumber = Float.parseFloat(mEdtSecondNumber.getText().toString());
-                    division(firstNumber, secondNumber);
-                }
-            }
-        });
+                division(firstNumber, secondNumber);
+                break;
+        }
     }
 
-    private void subtraction(float firstNumber, float secondNumber) {
-        mTvResult.setText(String.valueOf(firstNumber - secondNumber));
+    private void subtraction(BigDecimal firstNumber, BigDecimal secondNumber) {
+        mTvResult.setText(String.valueOf(firstNumber.subtract(secondNumber)));
     }
 
-    private void addition(float firstNumber, float secondNumber) {
-        final float result = firstNumber + secondNumber;
-        mTvResult.setText(String.valueOf(result));
+    private void addition(BigDecimal firstNumber, BigDecimal secondNumber) {
+        mTvResult.setText(String.valueOf(firstNumber.add(secondNumber)));
     }
 
-    private void division(float firstNumber, float secondNumber) {
-        if (Float.compare(secondNumber, 0) == 0) {
+    private void division(BigDecimal firstNumber, BigDecimal secondNumber) {
+        if (secondNumber.compareTo(new BigDecimal(0)) == 0) {
             Toast.makeText(this, "Error: divided by zero", Toast.LENGTH_LONG).show();
-            mTvResult.setText(String.valueOf(Float.MAX_VALUE));
             return;
         }
-        mTvResult.setText(String.valueOf(firstNumber / secondNumber));
+        BigDecimal resultOne = firstNumber.divide(secondNumber, 0, RoundingMode.DOWN);
+        BigDecimal resultTwo = firstNumber.divide(secondNumber, 5, RoundingMode.DOWN);
+        if (resultOne.compareTo(resultTwo) == 0) {
+            mTvResult.setText(String.valueOf(resultOne));
+        } else {
+            mTvResult.setText(String.valueOf(resultTwo));
+        }
     }
 
-    private void multiplication(float firstNumber, float secondNumber) {
-        mTvResult.setText(String.valueOf(firstNumber * secondNumber));
+    private void multiplication(BigDecimal firstNumber, BigDecimal secondNumber) {
+        mTvResult.setText(String.valueOf(firstNumber.multiply(secondNumber)));
     }
 
     private boolean checkInput() {
