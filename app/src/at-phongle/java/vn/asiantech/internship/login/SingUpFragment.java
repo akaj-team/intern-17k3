ab4c6ea74a3a86e9a72597acda618ff112331cf6
Phong Le T <phong.le@asiantech.vn>
2017-11-30 16:37:01 +0700

    fix ci

--- app/src/at-phongle/java/vn/asiantech/internship/login/SingUpFragment.java
+++ app/src/at-phongle/java/vn/asiantech/internship/login/SingUpFragment.java
@@ -1,6 +1,5 @@
 package vn.asiantech.internship.login;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
