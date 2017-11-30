01c3818663241a72b39bd18738404212e16ce093
at-nhatnguyen <nhat.nguyen@asiantech.vn>
2017-11-30 17:13:36 +0700

    change activity_login

--- app/src/at-nhatnguyen/java/vn/asiantech/internship/LoginActivity.java
+++ app/src/at-nhatnguyen/java/vn/asiantech/internship/LoginActivity.java
@@ -14,7 +14,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         mBtnBack = findViewById(R.id.btnBack);
         mBtnBack.setOnClickListener(this);
         if (getSupportActionBar() != null) {
