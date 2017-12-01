561db7c49f7473c6a898ba9c82cce9be4db65374
Tung Nguyen T <tung.nguyen2@asiantech.vn>
2017-12-01 16:17:07 +0700

    update fix Drawer

---
+++ app/src/at-tungnguyen/java/vn/asiantech/internship/DrawerActivity.java
@@ -0,0 +1,55 @@
+package vn.asiantech.internship;
+
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+        mToolbar = findViewById(R.id.toolbar);
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+        initDrawer();
+    }
+
+    private void initDrawer() {
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout,mToolbar,R.string.open,R.string.close){
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                Log.i("XXX", "onDrawerSlide: ");
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                Log.i("XXX", "onDrawerOpened: ");
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                Log.i("XXX", "onDrawerClosed: ");
+            }
+        };
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mActionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+}
