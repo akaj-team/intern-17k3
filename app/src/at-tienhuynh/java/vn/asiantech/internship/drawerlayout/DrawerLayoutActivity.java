a503922ef05f42d6647c605a1ba9f89156aa894a
at-tienhuynh3 <tien.huynh3@asiantech.vn>
2017-12-01 09:09:54 +0700

    add code

--- app/src/at-tienhuynh/java/vn/asiantech/internship/drawerlayout/DrawerLayoutActivity.java
+++ app/src/at-tienhuynh/java/vn/asiantech/internship/drawerlayout/DrawerLayoutActivity.java
@@ -2,7 +2,12 @@ package vn.asiantech.internship.drawerlayout;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import vn.asiantech.internship.R;
 
@@ -12,9 +17,53 @@ import vn.asiantech.internship.R;
  */
 
 public class DrawerLayoutActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawerlayout);
+        initViews();
+        if (getSupportActionBar() != null) {
+            setSupportActionBar(mToolbar);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+        callDrawerMenu();
     }
+
+    private void callDrawerMenu() {
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();
+    }
+
+    private void initViews() {
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+        mToolbar = findViewById(R.id.action_bar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mActionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+
 }
