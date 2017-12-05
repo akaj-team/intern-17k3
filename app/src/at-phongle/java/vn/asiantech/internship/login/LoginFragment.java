e9300f5a38542010081fa7c7b0a72d27d517da2d
Tung Nguyen T <tung.nguyen2@asiantech.vn>
2017-12-05 10:28:45 +0700

    update

--- app/src/at-phongle/java/vn/asiantech/internship/login/LoginFragment.java
+++ app/src/at-phongle/java/vn/asiantech/internship/login/LoginFragment.java
@@ -1,7 +1,6 @@
--package vn.asiantech.internship.login;
+package vn.asiantech.internship.login;
 
-
-        import android.graphics.Color;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
