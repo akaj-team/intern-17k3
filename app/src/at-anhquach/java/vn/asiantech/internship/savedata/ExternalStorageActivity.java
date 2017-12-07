9762c5ea353d5ec0a060efe9139fa45edfe54e97
Anh Quach T. N <anh.quach@asiantech.vn>
2017-12-07 09:35:24 +0700

    not yet

--- app/src/at-anhquach/java/vn/asiantech/internship/savedata/ExternalStorageActivity.java
+++ app/src/at-anhquach/java/vn/asiantech/internship/savedata/ExternalStorageActivity.java
@@ -1,10 +1,17 @@
 package vn.asiantech.internship.savedata;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 import vn.asiantech.internship.R;
 
@@ -17,10 +24,11 @@ public class ExternalStorageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_preference);
         initView();
+        readFileExternal();
         mBtnSaveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                saveData();
+                writeFileExternal(mEdtSaveData.getText().toString());
             }
         });
     }
@@ -30,8 +38,51 @@ public class ExternalStorageActivity extends AppCompatActivity {
         mBtnSaveData = findViewById(R.id.btnSaveData);
     }
 
-    private void saveData() {
-
+    private boolean isExternalReadable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
     }
 
+    private boolean isExternalWriteable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
+    private void writeFileExternal(String value) {
+        if (!isExternalWriteable()) {
+            // No-opp
+        } else {
+            File fileExternal = new File(Environment.getExternalStorageDirectory().getPath() + "/AnhQuach");
+            fileExternal.mkdirs();
+            File abc = new File(fileExternal.getPath(), "abc.txt");
+            try {
+                FileOutputStream outputStream = new FileOutputStream(abc);
+                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+                bufferedOutputStream.write(value.getBytes());
+
+                bufferedOutputStream.flush();
+                bufferedOutputStream.close();
+                outputStream.close();
+            } catch (IOException writeData) {
+                writeData.printStackTrace();
+            }
+        }
+    }
+
+    private void readFileExternal() {
+        if (!isExternalReadable()) {
+            String sdcard = Environment.getExternalStorageDirectory().getAbsolutePath() + "/AnhQuach" + "/abc.txt";
+            try {
+                Scanner scan = new Scanner(new File(sdcard));
+                StringBuilder data = new StringBuilder();
+                while (scan.hasNext()) {
+                    data.append(scan.nextLine());
+                }
+                scan.close();
+                mEdtSaveData.setText(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
