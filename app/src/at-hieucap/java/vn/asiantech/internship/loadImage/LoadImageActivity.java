03aad60c7c0543c2d91b75ad13d724bc9abd2719
hieucap <hieu.cap@asiantech.vn>
2017-12-13 09:01:42 +0700

    update load image

--- app/src/at-hieucap/java/vn/asiantech/internship/loadImage/LoadImageActivity.java
+++ app/src/at-hieucap/java/vn/asiantech/internship/loadImage/LoadImageActivity.java
@@ -1,38 +1,69 @@
 package vn.asiantech.internship.loadImage;
 
-import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 import vn.asiantech.internship.R;
 
-public class LoadImageActivity extends AppCompatActivity {
-    private ImageAdapter mAdapter;
-    private List<ItemImage> mList = new ArrayList<>();
-    private RecyclerView mRecyclerView;
-    private ImageView mImgItem;
-    private Context mContext;
-
-    public LoadImageActivity(ImageView mImgItem, Context mContext) {
-        this.mContext = mContext;
-    }
-
+public class LoadImageActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView mImgContainImage;
+    private Button mBtnChoiceDrawable;
+    private Button mBtnChoiceAssets;
+    private Button mBtnChoiceStorage;
+    private Button mBtnChoiceInternet;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mRecyclerView = findViewById(R.id.rlImage);
-        mAdapter = new ImageAdapter(mList);
-        mRecyclerView.setAdapter(mAdapter);
-        mImgItem = findViewById(R.id.imgItem);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        mList.add(new ItemImage(Picasso.with(this).load("http://i.imgur.com/DvpvklR.png")));
+        setContentView(R.layout.activity_load_image);
+        initViews();
+        addListener();
+    }
+
+    private void initViews() {
+        mImgContainImage = findViewById(R.id.imgContainImage);
+        mBtnChoiceDrawable = findViewById(R.id.btnDrawable);
+        mBtnChoiceAssets = findViewById(R.id.btnAsset);
+        mBtnChoiceStorage = findViewById(R.id.btnStorage);
+        mBtnChoiceInternet = findViewById(R.id.btnInternet);
+    }
+
+    private void addListener() {
+        mBtnChoiceDrawable.setOnClickListener(this);
+        mBtnChoiceAssets.setOnClickListener(this);
+        mBtnChoiceStorage.setOnClickListener(this);
+        mBtnChoiceInternet.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnDrawable:
+                Picasso.with(this).load(R.drawable.ic_pogba).placeholder(R.mipmap.ic_launcher).into(mImgContainImage);
+                break;
+            case R.id.btnAsset:
+                Picasso.with(view.getContext()).load("file:///android_asset/img_ibrahimovich.jpg").into(mImgContainImage);
+                break;
+            case R.id.btnStorage:
+                File imgFile = new  File("/sdcard/Pictures/Messenger/Ahihi.jpeg");
+//                Uri uri = Uri.fromFile(new File(Environment.getExternalStorageDirectory().getPath() + "/sdcard/Pictures/Messenger/Ahihi.jpeg", "ic_image_storage.png"));
+//                Picasso.with(view.getContext()).load(uri).into(mImgContainImage);
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                mImgContainImage.setImageBitmap(myBitmap);
+                break;
+            case R.id.btnInternet:
+                Picasso.with(view.getContext()).load("https://i2-prod.manchestereveningnews.co.uk/incoming/article1736623.ece/ALTERNATES/s1227b/Paul%20Scholes.jpg").into(mImgContainImage);
+                break;
+        }
     }
 }
