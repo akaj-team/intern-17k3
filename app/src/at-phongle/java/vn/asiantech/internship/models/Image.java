0e4d5f09dc888571ce2f900cd52fcb64631da31e
Phong Le T <phong.le@asiantech.vn>
2017-12-13 12:10:03 +0700

    pr image

---
+++ app/src/at-phongle/java/vn/asiantech/internship/models/Image.java
@@ -0,0 +1,42 @@
+package vn.asiantech.internship.models;
+
+/**
+ * Created by phongle on 12/12/2560.
+ * model Image
+ */
+
+public class Image {
+    private String imageUrl;
+    private int imageDrawerble;
+    private String information;
+
+    public Image(String imageUrl, int imageDrawerble, String information) {
+        this.imageUrl = imageUrl;
+        this.imageDrawerble = imageDrawerble;
+        this.information = information;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getImageDrawerble() {
+        return imageDrawerble;
+    }
+
+    public void setImageDrawerble(int imageDrawerble) {
+        this.imageDrawerble = imageDrawerble;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+}
