d90e231a453f01bbb3be9621cb7bd72b32fa0612
at-tienhuynh3 <tien.huynh3@asiantech.vn>
2017-12-13 08:21:53 +0700

    first init

---
+++ app/src/at-tienhuynh/java/vn/asiantech/internship/image/NinePathActivity.java
@@ -0,0 +1,15 @@
+package vn.asiantech.internship.image;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import vn.asiantech.internship.R;
+
+public class NinePathActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nine_path);
+    }
+}
