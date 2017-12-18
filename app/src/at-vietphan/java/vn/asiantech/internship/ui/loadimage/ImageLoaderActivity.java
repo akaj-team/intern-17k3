75b36cc32ef83f7fa655f98e75692fdd61e655e9
Viet Phan X <viet.phan@asiantech.vn>
2017-12-18 11:44:05 +0700

    reformat code

--- app/src/at-vietphan/java/vn/asiantech/internship/ui/loadimage/ImageLoaderActivity.java
+++ app/src/at-vietphan/java/vn/asiantech/internship/ui/loadimage/ImageLoaderActivity.java
@@ -32,7 +32,7 @@ public class ImageLoaderActivity extends AppCompatActivity implements View.OnCli
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_load_image);
+        setContentView(R.layout.activity_image_loader);
         initViews();
         initListener();
     }
