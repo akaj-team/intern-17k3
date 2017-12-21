1b9b89e16153cd19e5d909d63e5f79c4bb34692e
Phong Le T <phong.le@asiantech.vn>
2017-12-21 13:52:42 +0700

    save

---
+++ app/src/at-phongle/java/vn/asiantech/internship/asynchronous/CountdownTimerActivity.java
@@ -0,0 +1,12 @@
+package vn.asiantech.internship.asynchronous;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by phongle on 21/12/2560.
+ * Activity CountdownTimer recycler view
+ */
+
+public class CountdownTimerActivity extends AppCompatActivity {
+
+}
