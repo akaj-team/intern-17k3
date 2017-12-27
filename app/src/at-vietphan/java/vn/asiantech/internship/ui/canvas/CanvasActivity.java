cb847a7d33e5daf2b4ed824fb5467e7cd4860168
Viet Phan X <viet.phan@asiantech.vn>
2017-12-27 18:11:38 +0700

    done version 1

--- app/src/at-vietphan/java/vn/asiantech/internship/ui/canvas/CanvasActivity.java
+++ app/src/at-vietphan/java/vn/asiantech/internship/ui/canvas/CanvasActivity.java
@@ -3,7 +3,6 @@ package vn.asiantech.internship.ui.canvas;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.LinearLayout;
 
 import vn.asiantech.internship.R;
 
@@ -11,14 +10,11 @@ import vn.asiantech.internship.R;
  * Created by vietphan on 25/12/2017.
  * Class CanvasActivity
  */
-
 public class CanvasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
-        CustomView customView = new CustomView(this);
-        ((LinearLayout) findViewById(R.id.llCanvas)).addView(customView);
     }
 }
--- app/src/at-vietphan/java/vn/asiantech/internship/ui/canvas/CustomView.java
+++ app/src/at-vietphan/java/vn/asiantech/internship/ui/canvas/CustomView.java
@@ -1,5 +1,6 @@
 package vn.asiantech.internship.ui.canvas;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,7 +9,6 @@ import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
@@ -22,17 +22,27 @@ import vn.asiantech.internship.R;
  * Class CustomView
  */
 public class CustomView extends View {
+    private static final int STROKE_WIDTH_LINE = 28;
+    private static final int STROKE_WIDTH_LINE_DISTANCE = 2;
+    private static final int START_LINE_DISTANCE = 120;
+    private static final int START_TEXT_DISTANCE = 10;
+    private static final int TEXT_BETWEEN_LINE = 10;
+    private static final int RESIDUAL_DISTANCE = 13;
     private Paint mPaintA;
     private Paint mPaintB;
     private Paint mPaintC;
-    private Paint mPaintNumber;
     private Paint mPaintMonth;
-    private Paint mPaintLineMaxDistance;
-    private Paint mPaintLineMinDistance;
-    private Paint mPaintLineBetweenDistance;
+    private Paint mPaintDistance;
+    private Paint mPaintLineDistance;
+    private Paint mPaintLeftBox;
     private int[] mPersonA;
     private int[] mPersonB;
     private int[] mPersonC;
+    private float mMoveX;
+    private float mTouchX;
+    private int mStartXPersonA;
+    private int mStartXPersonB;
+    private int mStartXPersonC;
     private ScaleGestureDetector mScaleDetector;
     private float mScaleFactor = 1.f;
 
@@ -42,65 +52,70 @@ public class CustomView extends View {
 
     public CustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initViews();
         mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+        initViews();
+        initData();
     }
 
     private void initViews() {
 //        mPaintA
         mPaintA = new Paint();
         mPaintA.setColor(Color.RED);
-        mPaintA.setStrokeWidth(18);
+        mPaintA.setStrokeWidth(STROKE_WIDTH_LINE);
         mPaintA.setStrokeCap(Paint.Cap.ROUND);
         mPaintA.setAntiAlias(true);
 //        mPaintB
         mPaintB = new Paint();
         mPaintB.setColor(Color.BLUE);
-        mPaintB.setStrokeWidth(18);
+        mPaintB.setStrokeWidth(STROKE_WIDTH_LINE);
         mPaintB.setStrokeCap(Paint.Cap.ROUND);
         mPaintB.setAntiAlias(true);
 //        mPaintC
         mPaintC = new Paint();
         mPaintC.setColor(Color.GREEN);
-        mPaintC.setStrokeWidth(18);
+        mPaintC.setStrokeWidth(STROKE_WIDTH_LINE);
         mPaintC.setStrokeCap(Paint.Cap.ROUND);
         mPaintC.setAntiAlias(true);
-//        mPaintNumber
-        mPaintNumber = new Paint();
-        mPaintNumber.setColor(Color.BLACK);
-        mPaintNumber.setTextSize(22);
 //        mPaintMonth
         mPaintMonth = new Paint();
-        mPaintMonth.setColor(Color.BLACK);
-        mPaintMonth.setTextSize(22);
-//        mPaintLineMaxDistance
-        mPaintLineMaxDistance = new Paint();
-        mPaintLineMaxDistance.setStrokeWidth(2);
-        mPaintLineMaxDistance.setColor(Color.GRAY);
-//        mPaintLineMinDistance
-        mPaintLineMinDistance = new Paint();
-        mPaintLineMinDistance.setStrokeWidth(2);
-        mPaintLineMinDistance.setColor(Color.GRAY);
-//        mPaintLineBetweenDistance
-        mPaintLineBetweenDistance = new Paint();
-        mPaintLineBetweenDistance.setStrokeWidth(2);
-        mPaintLineBetweenDistance.setColor(Color.GRAY);
+        mPaintMonth.setColor(Color.DKGRAY);
+        mPaintMonth.setTextSize(getResources().getDimensionPixelSize(R.dimen.text_size_chart));
+//        mPaintDistance
+        mPaintDistance = new Paint();
+        mPaintDistance.setColor(Color.DKGRAY);
+        mPaintDistance.setTextSize(getResources().getDimensionPixelSize(R.dimen.text_size_chart));
+//        mPaintLineDistance
+        mPaintLineDistance = new Paint();
+        mPaintLineDistance.setStrokeWidth(STROKE_WIDTH_LINE_DISTANCE);
+        mPaintLineDistance.setColor(Color.GRAY);
+//        mPaintLeftBox
+        mPaintLeftBox = new Paint();
+        mPaintLeftBox.setColor(Color.WHITE);
+    }
+
+    private void initData() {
+        mStartXPersonA = 100;
+        mStartXPersonB = 130;
+        mStartXPersonC = 160;
+        mPersonA = getResources().getIntArray(R.array.person_one);
+        mPersonB = getResources().getIntArray(R.array.person_two);
+        mPersonC = getResources().getIntArray(R.array.person_three);
     }
 
     private int setStartX(int startX) {
-        return startX * 80 + 100;
+        return startX * 150;
     }
 
     private int setStartY() {
         return this.getHeight() / 2;
     }
 
-    private int setStopX(int stopX) {
-        return stopX * 80 + 100;
+    private int setStopY(int stopY) {
+        return stopY * 50;
     }
 
-    private int setStopY(int stopY) {
-        return stopY * 30;
+    private int setStartYText() {
+        return setStartY() + 60;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -109,43 +124,55 @@ public class CustomView extends View {
         super.onDraw(canvas);
         canvas.save();
         canvas.scale(mScaleFactor, mScaleFactor);
-        mPersonA = getResources().getIntArray(R.array.person_one);
-        mPersonB = getResources().getIntArray(R.array.person_two);
-        mPersonC = getResources().getIntArray(R.array.person_three);
-        getMaxValueDistance();
-        getMinValueDistance();
-        Log.d("l", "list: " + mPersonA.length);
-        for (int i = 0; i < mPersonA.length; i++) {
-//            Draw mPersonA
-            Log.d("3", "1: " + mPersonA[i]);
-            canvas.drawLine(setStartX(i), setStartY(), setStopX(i), setStartY() - setStopY(mPersonA[i]), mPaintA);
-            canvas.drawText(String.valueOf(mPersonA[i]), setStartX(i) - 5, setStartY() + 30, mPaintNumber);
-//            Draw mPersonB
-            Log.d("3", "2: " + mPersonB[i]);
-            canvas.drawLine(setStartX(i) + 20, setStartY(), setStopX(i) + 20, setStartY() - setStopY(mPersonB[i]), mPaintB);
-            canvas.drawText(String.valueOf(mPersonB[i]), setStartX(i) + 20 - 5, setStartY() + 30, mPaintNumber);
-//            Draw mPersonC
-            Log.d("3", "3: " + mPersonC[i]);
-            canvas.drawLine(setStartX(i) + 40, setStartY(), setStopX(i) + 40, setStartY() - setStopY(mPersonC[i]), mPaintC);
-            canvas.drawText(String.valueOf(mPersonC[i]), setStartX(i) + 40 - 5, setStartY() + 30, mPaintNumber);
-//            Draw lineDistance max, min, between
-            canvas.drawLine(100, setStartY() - setStopY(getMaxValueDistance()), getWidth(), setStartY() - setStopY(getMaxValueDistance()), mPaintLineMaxDistance);
-            canvas.drawLine(100, setStartY(), getWidth(), setStartY(), mPaintLineMinDistance);
-            canvas.drawLine(100, setStartY() - setStopY(getMaxValueDistance()) / 2, getWidth(), setStartY() - setStopY(getMaxValueDistance()) / 2, mPaintLineBetweenDistance);
-//            Draw mPaintMonth
-            if (i == 0) {
-                canvas.drawText("24 Jul", setStartX(0) - 5, setStartY() + 70, mPaintMonth);
-            }
-            if (i == mPersonA.length - 1) {
-                canvas.drawText("9 Oct", setStartX(mPersonA.length - 1), setStartY() + 70, mPaintMonth);
-            }
-        }
-        canvas.drawText("0 Km", 10, setStartY(), mPaintMonth);
-        canvas.drawText("9 Km", 10, setStartY() - setStopY(getMaxValueDistance()), mPaintMonth);
+        drawLineDistance(canvas);
+        drawLinePerson(canvas);
+        drawLeftBox(canvas);
+        drawText(canvas);
         canvas.restore();
     }
 
-    //Find maximum (largest) value in array using array sort
+    //    Draw left box for hint chart left to left screen
+    private void drawLeftBox(Canvas canvas) {
+        canvas.drawRect(0, 0, 120, getHeight(), mPaintLeftBox);
+    }
+
+    //  Draw line person A, B, C
+    private void drawLinePerson(Canvas canvas) {
+        for (int i = 0; i < mPersonA.length; i++) {
+            int startXA = mStartXPersonA + setStartX(i);
+            int startXB = mStartXPersonB + setStartX(i);
+            int startXC = mStartXPersonC + setStartX(i);
+            int startY = setStartY() - RESIDUAL_DISTANCE;
+            int stopYA = setStartY() - setStopY(mPersonA[i]) + RESIDUAL_DISTANCE;
+            int stopYB = setStartY() - setStopY(mPersonB[i]) + RESIDUAL_DISTANCE;
+            int stopYC = setStartY() - setStopY(mPersonC[i]) + RESIDUAL_DISTANCE;
+            canvas.drawLine(startXA, startY, startXA, stopYA, mPaintA);
+            canvas.drawLine(startXB, startY, startXB, stopYB, mPaintB);
+            canvas.drawLine(startXC, startY, startXC, stopYC, mPaintC);
+        }
+    }
+
+    //   Draw lineDistance max, min, between
+    private void drawLineDistance(Canvas canvas) {
+        int maxDistance = getMaxValueDistance();
+        int startYLineTop = setStartY() - setStopY(maxDistance);
+        int startYLineBetween = setStartY() - setStopY(maxDistance) / 2;
+        canvas.drawLine(START_LINE_DISTANCE, startYLineTop, getWidth(), startYLineTop, mPaintLineDistance);
+        canvas.drawLine(START_LINE_DISTANCE, startYLineBetween, getWidth(), startYLineBetween, mPaintLineDistance);
+        canvas.drawLine(START_LINE_DISTANCE, setStartY(), getWidth(), setStartY(), mPaintLineDistance);
+    }
+
+    private void drawText(Canvas canvas) {
+        canvas.drawText("24 Jul", START_LINE_DISTANCE, setStartYText(), mPaintMonth);
+        canvas.drawText("9 Oct", getWidth() - 90, setStartYText(), mPaintMonth);
+        canvas.drawText("0 Km", START_TEXT_DISTANCE, setStartY() + TEXT_BETWEEN_LINE, mPaintDistance);
+        canvas.drawText(getMaxValueDistance() + " Km", START_TEXT_DISTANCE,
+                setStartY() - setStopY(getMaxValueDistance()) + TEXT_BETWEEN_LINE, mPaintDistance);
+    }
+
+    /**
+     * Find maximum (largest) value in array using array sort
+     */
     private int getMaxValueDistance() {
         Arrays.sort(mPersonA);
         Arrays.sort(mPersonB);
@@ -153,33 +180,36 @@ public class CustomView extends View {
         int maxA = mPersonA[mPersonA.length - 1];
         int maxB = mPersonB[mPersonB.length - 1];
         int maxC = mPersonC[mPersonC.length - 1];
-        int maxDistance = Math.max(maxA, Math.max(maxB, maxC));
-        Log.d("max", "getMaxValueDistance: " + maxDistance);
-        return maxDistance;
+        return Math.max(maxA, Math.max(maxB, maxC));
     }
 
-    //Find maximum (largest) value in array using array sort
-    private int getMinValueDistance() {
-        Arrays.sort(mPersonA);
-        Arrays.sort(mPersonB);
-        Arrays.sort(mPersonC);
-        int minA = mPersonA[0];
-        int minB = mPersonB[0];
-        int minC = mPersonC[0];
-        int minDistance = Math.min(minA, Math.min(minB, minC));
-        Log.d("max", "getMinValueDistance: " + minDistance);
-        return minDistance;
-    }
-
+    @SuppressLint("ClickableViewAccessibility")
     @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        // Let the ScaleGestureDetector inspect all events.
-        mScaleDetector.onTouchEvent(ev);
+    public boolean onTouchEvent(MotionEvent event) {
+        mScaleDetector.onTouchEvent(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mTouchX = event.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (event.getPointerCount() == 1) {
+                    mMoveX = event.getX() - mTouchX + mMoveX;
+                    mTouchX = event.getX();
+                    updateStartXPersonLine(mMoveX);
+                }
+                break;
+        }
+        invalidate();
         return true;
     }
 
-    private class ScaleListener
-            extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+    private void updateStartXPersonLine(float moveX) {
+        mStartXPersonA = (int) (100 + moveX);
+        mStartXPersonB = (int) (130 + moveX);
+        mStartXPersonC = (int) (160 + moveX);
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             mScaleFactor *= detector.getScaleFactor();
@@ -189,5 +219,4 @@ public class CustomView extends View {
             return true;
         }
     }
-
 }
