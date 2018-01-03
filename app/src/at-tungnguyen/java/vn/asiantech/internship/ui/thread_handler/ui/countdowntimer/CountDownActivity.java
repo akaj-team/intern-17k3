b20fcee9c397943832a01cd66dcd06891512ff60
Tung Nguyen T <tung.nguyen2@asiantech.vn>
2018-01-03 09:53:54 +0700

    Fix convention

--- app/src/at-tungnguyen/java/vn/asiantech/internship/ui/thread_handler/ui/countdowntimer/CountDownActivity.java
+++ app/src/at-tungnguyen/java/vn/asiantech/internship/ui/thread_handler/ui/countdowntimer/CountDownActivity.java
@@ -76,7 +76,7 @@ public class CountDownActivity extends AppCompatActivity {
             @Override
             public void onTick(long l) {
                 mCount = (int) (l / 1000);
-                mTvCountDown.setText(String.valueOf("T"));
+                mTvCountDown.setText(String.valueOf(mCount));
 
                 ++mAddItem;
                 ++mDeleteItem;
