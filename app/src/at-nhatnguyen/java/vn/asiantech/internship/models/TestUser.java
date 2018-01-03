8754f7db3bb004ef10813aee96cbc4ff4b62913d
at-nhatnguyen <nhat.nguyen@asiantech.vn>
2018-01-03 17:08:10 +0700

    complete unit test

---
+++ app/src/at-nhatnguyen/java/vn/asiantech/internship/models/TestUser.java
@@ -0,0 +1,26 @@
+package vn.asiantech.internship.models;
+
+/**
+ * Created by hoangnhat on 03/01/2018.
+ */
+
+public class TestUser {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
