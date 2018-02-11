5ee4c1a76f79d6d7d7f1887d3ddcdc0d7b338d2a
hieucap <hieu.cap@asiantech.vn>
2018-02-11 22:03:40 +0700

    update

--- app/src/at-hieucap/java/vn/asiantech/internship/ui/kotlinlogin/ViewPagerAdapter.kt
+++ app/src/at-hieucap/java/vn/asiantech/internship/ui/kotlinlogin/ViewPagerAdapter.kt
@@ -8,7 +8,7 @@ import vn.asiantech.internship.models.CardLogin
  * Created by tiboo on 05/02/2018.
  * Create fragment state pager adapter
  */
-class gitViewPagerAdapter(val fragmentManager: FragmentManager, private val listCardLogin: ArrayList<CardLogin>) : FragmentStatePagerAdapter(fragmentManager) {
+class ViewPagerAdapter(val fragmentManager: FragmentManager, private val listCardLogin: ArrayList<CardLogin>) : FragmentStatePagerAdapter(fragmentManager) {
     override fun getItem(position: Int): StepLoginFragment {
         return StepLoginFragment.newInstance(listCardLogin[position])
     }
